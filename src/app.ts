47547c60ef2be58db775169426df1e836b6f2c7a
Eric Afes <eriicafes@gmail.com>
2022-07-17 19:11:48 +0100

    feat: add utils and export actions

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
+import { actions } from "./utils";
+
 console.log("Hello world!");
 console.log("Hello reflow!");
 console.log("first change");
+
+actions.talk();
+actions.walk();
---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export const actions = {
+  talk() {
+    console.log("talking");
+  },
+  walk() {
+    console.log("walking");
+  },
+};
